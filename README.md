64fc0f845a2e3b3c5886883247fbb033c32a0283
Ghassan Qasrawi <gqasrawig@gmail.com>
2025-02-21 13:58:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hardwaresimu_software_graduation_project
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/hardwaresimu_software_graduation_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hardwaresimu_software_graduation_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
