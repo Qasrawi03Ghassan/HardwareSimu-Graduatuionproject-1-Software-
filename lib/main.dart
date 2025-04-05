12b9801a27165bd6aff13bbc993358b103d61b7f
Qasrawi03Ghassan <gqasrawig@gmail.com>
2025-04-05 14:34:29 +0300

    last changes before trying to update flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-import 'theme.dart';
+import 'package:hardwaresimu_software_graduation_project/theme.dart';
 import 'package:hardwaresimu_software_graduation_project/welcome.dart';
 
 Future<void> main() async {
@@ -13,13 +12,8 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  if (kIsWeb) {
-    runApp(const WebApp());
-  } else if (Platform.isAndroid || Platform.isIOS) {
-    runApp(const MainApp());
-  } else {
-    //Do nothing
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -28,10 +22,15 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "CircuitAcademy",
       theme: SysThemes().lightTheme,
       darkTheme: SysThemes().darkTheme,
       themeMode: ThemeMode.system,
-      home: WelcomePage(),
+      home: kIsWeb
+          ? const WebApp() //Running on web
+          : (Platform.isAndroid || Platform.isIOS)
+              ? WelcomePage() //Running on mobile
+              : null, //Running on desktop which we wont implement
     );
   }
 }
@@ -41,10 +40,21 @@ class WebApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Center(
-        child: Text("Hello web app!"),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "CircuitAcademy",
+            style: TextStyle(fontSize: 30),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.red,
+            height: 200,
+            width: 200,
+            child: TextButton(onPressed: () {}, child: Text("data")),
+          ),
+        ));
   }
 }
